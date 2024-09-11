55717880a1cd8b29a651a56e2c024d5bcce49035
aminul-islam <aminulislamshykat@gmail.com>
2024-09-12 00:56:46 +0600

    adding some packeges

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,17 @@
 const express = require("express");
+const cors = require("cors");
+
+// App initialize 
 const app = express();
 
+// Basic middlewares 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
 
+app.get("/",(req,res)=>{
+  res.status(200).send("Express API is running")
+})
 
 
 
