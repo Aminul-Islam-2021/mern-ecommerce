c455d35986de179568b2afe76d98b48c62108a3f
aminul-islam <aminulislamshykat@gmail.com>
2024-09-11 15:42:11 +0600

    Adding some packeges and create app

--- server/src/server.js
+++ server/src/server.js
@@ -1,1 +1,7 @@
-console.log("server is running")
+const app = require("./app");
+require("dotenv").config();
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+  console.log(`server is running on port:${port}`)
+})
